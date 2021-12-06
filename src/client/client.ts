b21f80bb4b5aaafee0c38eee8a4db075562906ed
arslonbekXX <arslonbek.lion067@gmail.com>
2021-12-06 10:22:10 +0500

    add init three codes

---
+++ src/client/client.ts
@@ -0,0 +1,48 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 2
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshNormalMaterial({
+    wireframe: false,
+})
+
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+
+let speedX = 0
+let speedY = 0
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    cube.rotation.x += speedX
+    cube.rotation.y += speedY
+
+    controls.update()
+
+    render()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+animate()
